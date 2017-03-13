c979215f3c2e8310721341714b8b66f27cf1810f
Brian Gereke <bgereke@utexas.edu>
2017-03-13 16:13:24 -0500

    Update baseline_by_day.m

--- imaging/baseline_by_day.m
+++ imaging/baseline_by_day.m
@@ -12,11 +12,12 @@ numdays = size(dates,1);
 for d = 1:numdays
     mcount = 0;
     for m = 1:nummice
+        data = [];
         filestart = strcat(mice(m,:),'_',dates(d,:));
         %check for existence and load calcium data
         exists = 0;
         cd(imdir)
-        imfiles = dir('*.mat');
+        imfiles = dir('*signals.mat');
         for f = 1:numel(imfiles)
             if ~isempty(regexp(imfiles(f).name,filestart))
                %load raw data
@@ -25,25 +26,11 @@ for d = 1:numdays
                %do baseline correction and transient detection
                [dF,nF,zF] = deltaF(raw,4);
                [dT,nT,zT] = detect_trans(dF,nF,zF);
-               %save all data to .csv tables
-               dFtable = array2table(dF);
-               dFname = char(strcat(filestart,'_dF.csv'));
-               writetable(dFtable,dFname)
-               nFtable = array2table(nF);
-               nFname = char(strcat(filestart,'_nF.csv'));
-               writetable(nFtable,nFname)
-               zFtable = array2table(zF);
-               zFname = char(strcat(filestart,'_zF.csv'));
-               writetable(zFtable,zFname)
-               dTtable = array2table(dT);
-               dTname = char(strcat(filestart,'_dT.csv'));
-               writetable(dTtable,dTname)
-               nTtable = array2table(nT);
-               nTname = char(strcat(filestart,'_nT.csv'));
-               writetable(nTtable,nTname)
-               zTtable = array2table(zT);
-               zTname = char(strcat(filestart,'_zT.csv'));
-               writetable(zTtable,zTname)
+               %save all data to .mat struct
+               data.dF = dF;data.nF = nF;data.zF = zF;
+               data.dT = dT;data.nT = nT;data.zT = zT;               
+               filename = char(strcat(filestart,'.mat'));
+               save(filename,'-struct','data');
             end
         end                
     end
