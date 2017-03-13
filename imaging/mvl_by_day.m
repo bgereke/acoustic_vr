b965d63bf181f0ca7cfc434124032fe8f0a73025
Brian Gereke <bgereke@utexas.edu>
2017-03-13 16:14:00 -0500

    Update mvl_by_day.m

--- imaging/mvl_by_day.m
+++ imaging/mvl_by_day.m
@@ -1,7 +1,8 @@
 %mean vector length distributions across days
 
 mice = string(['md229';'md230';'md231']);
-dates = string(['0226';'0227';'0228';'0301';'0302';'0303';'0304']);
+dates = string(['02262017';'02272017';'02282017';'03012017';...
+                '03022017';'03032017';'03042017']);
 imdir = 'Z:\imaging\wheel_run_5\signals\';
 rundir = 'Z:\imaging\wheel_run_5\rpm\';
 
@@ -22,13 +23,13 @@ for d = 1:numdays
     mcount = 0;
     for m = 1:nummice
         filestart = strcat(mice(m,:),'_',dates(d,:));
-        %check for existence and load calcium data
+        %check for existence and load detected transients
         exists = 0;
         cd(imdir)
-        imfiles = dir('*.mat');
+        imfiles = dir('*.mat'); 
         for f = 1:numel(imfiles)
             if ~isempty(regexp(imfiles(f).name,filestart))
-               load(imfiles(f).name)
+               load(imfiles(f).name,'-mat','nT');%add variables to load
                exists = 1;
             end
         end
@@ -44,14 +45,13 @@ for d = 1:numdays
             end
             %get mvl for day mouse combo
             cdt=1/10.088781275221955;
-            raw = double(squeeze(raw)');
-            ct = cdt*ones(size(raw,1),1);
+            ct = cdt*ones(size(nt,1),1);
             ct = cumsum(ct);
             ard_timestamp = (runtable.ard_timestamp - min(runtable.ard_timestamp))/1000 + cdt;
             wheel_position = runtable.lap_position/64;
             wheel_position = wheel_position - floor(wheel_position);
-            [dF,nF,zF] = deltaF(raw,2);
-            [maps,pmap,grid,mvl,dbmvl,mvlp,bpsl,bptl,ftrans] = ratemap(nF,runtable.lap_position,ct,ard_timestamp,100,'nthresh','vonMises',30);
+%             [dF,nF,zF] = deltaF(raw,2);
+            [maps,pmap,grid,mvl,dbmvl,mvlp,bpsl,bptl] = ratemap(nT,runtable.lap_position,ct,ard_timestamp,100,'vonMises',30,false);
             MVL_d(d,:) = MVL_d(d,:) + hist(dbmvl,hgrid);
             MVLp_d(d,:) = MVLp_d(d,:) + hist(mvlp,pgrid);
             DBMVL{mcount} = dbmvl;
